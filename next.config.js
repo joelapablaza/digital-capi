b36596a7db45aea9551564b1071d6b99e820af6e
joelapablaza <joelapablaza@gmail.com>
2023-12-04 13:08:47 -0300

    fix: strapi key

--- next.config.js
+++ next.config.js
@@ -1,10 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // images: {
-  //   // this is technically deprecated but works perfectly.
-  //   // if you'd like to see the remotePatterns version, I'm happy to merge a PR
-  //   domains: ["localhost", "digital-capi-production.up.railway.app"],
-  // },
   images: {
     remotePatterns: [
       {
