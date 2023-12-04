4048a3e9bce4900ef672290ae27f69b2f212d35e
joelapablaza <joelapablaza@gmail.com>
2023-12-04 02:17:33 -0300

    fix: changed 'images.domains' (deprecated) to 'images.remotePatterns'

--- next.config.js
+++ next.config.js
@@ -1,9 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // images: {
+  //   // this is technically deprecated but works perfectly.
+  //   // if you'd like to see the remotePatterns version, I'm happy to merge a PR
+  //   domains: ["localhost", "digital-capi-production.up.railway.app"],
+  // },
   images: {
-    // this is technically deprecated but works perfectly.
-    // if you'd like to see the remotePatterns version, I'm happy to merge a PR
-    domains: ["localhost", "digital-capi-production.up.railway.app"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "digital-capi-production.up.railway.app",
+        pathname: "**",
+      },
+    ],
   },
 };
 
