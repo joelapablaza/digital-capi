08c632c34064c977cde877f20d0316ad4e28c877
joelapablaza <joelapablaza@gmail.com>
2023-12-08 02:25:50 -0300

    resend api key fixed

--- src/get-payload.ts
+++ src/get-payload.ts
@@ -1,19 +1,19 @@
-import dotenv from "dotenv";
-import path from "path";
-import type { InitOptions } from "payload/config";
-import payload, { Payload } from "payload";
-import nodemailer from "nodemailer";
+import dotenv from 'dotenv';
+import path from 'path';
+import type { InitOptions } from 'payload/config';
+import payload, { Payload } from 'payload';
+import nodemailer from 'nodemailer';
 
 dotenv.config({
-  path: path.resolve(__dirname, "../.env"),
+  path: path.resolve(__dirname, '../.env'),
 });
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.resend.com",
+  host: 'smtp.resend.com',
   secure: true,
   port: 465,
   auth: {
-    user: "resend",
+    user: 'resend',
     pass: process.env.RESEND_API_KEY,
   },
 });
@@ -35,7 +35,7 @@ export const getPayloadClient = async ({
   initOptions,
 }: Args = {}): Promise<Payload> => {
   if (!process.env.PAYLOAD_SECRET) {
-    throw new Error("PAYLOAD_SECRET is missing");
+    throw new Error('PAYLOAD_SECRET is missing');
   }
 
   if (cached.client) {
@@ -46,8 +46,8 @@ export const getPayloadClient = async ({
     cached.promise = payload.init({
       email: {
         transport: transporter,
-        fromAddress: "onboarding@resend.dev", // Configurar mi email de hello@joelapablaza.com.ar
-        fromName: "Digital-Capi",
+        fromAddress: 'digital-capi@joelapablaza.com.ar', // Configurar mi email de hello@joelapablaza.com.ar
+        fromName: 'Digital-Capi',
       },
       secret: process.env.PAYLOAD_SECRET,
       local: initOptions?.express ? false : true,
