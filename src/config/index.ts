936ac898583758d6abf8cebb1e8fc2f13d7a22a1
joelapablaza <joelapablaza@gmail.com>
2023-12-08 09:05:42 -0300

    style: header  images optimized

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,43 +1,43 @@
 export const PRODUCT_CATEGORIES = [
   {
-    label: "UI Kits",
-    value: "ui_kits" as const,
+    label: 'UI Kits',
+    value: 'ui_kits' as const,
     featured: [
       {
-        name: "Selecciones del editor",
+        name: 'Selecciones del editor',
         href: `/products?category=ui_kits`,
-        imageSrc: "/nav/ui-kits/mixed.jpg",
+        imageSrc: '/nav/ui-kits/mixed.webp',
       },
       {
-        name: "Novedades",
-        href: "/products?category=ui_kits&sort=desc",
-        imageSrc: "/nav/ui-kits/blue.jpg",
+        name: 'Novedades',
+        href: '/products?category=ui_kits&sort=desc',
+        imageSrc: '/nav/ui-kits/blue.webp',
       },
       {
-        name: "Más vendidos",
-        href: "/products?category=ui_kits",
-        imageSrc: "/nav/ui-kits/purple.jpg",
+        name: 'Más vendidos',
+        href: '/products?category=ui_kits',
+        imageSrc: '/nav/ui-kits/purple.webp',
       },
     ],
   },
   {
-    label: "Iconos",
-    value: "icons" as const,
+    label: 'Iconos',
+    value: 'icons' as const,
     featured: [
       {
-        name: "Selecciones de íconos favoritos",
+        name: 'Selecciones de íconos favoritos',
         href: `/products?category=icons`,
-        imageSrc: "/nav/icons/picks.jpg",
+        imageSrc: '/nav/icons/picks.webp',
       },
       {
-        name: "Novedades",
-        href: "/products?category=icons&sort=desc",
-        imageSrc: "/nav/icons/new.jpg",
+        name: 'Novedades',
+        href: '/products?category=icons&sort=desc',
+        imageSrc: '/nav/icons/new.webp',
       },
       {
-        name: "Iconos Más vendidos",
-        href: "/products?category=icons",
-        imageSrc: "/nav/icons/bestsellers.jpg",
+        name: 'Iconos Más vendidos',
+        href: '/products?category=icons',
+        imageSrc: '/nav/icons/bestsellers.webp',
       },
     ],
   },
